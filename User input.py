4df20c7a48b5d3e68baf12e787bef236d25a1817
FaisalAhmed64 <mdfaisalahmed025@gmail.com>
2021-10-21 23:20:07 +0600

    taking userinput

---
+++ User input.py
@@ -0,0 +1,12 @@
+name_is = input("what is your name? ")
+print("hi " + name_is)
+
+name = input("What is your name? ")
+Color = input("what is your favourite color? ")
+
+print(name + " likes " + Color)
+
+birth_date = input("birth year: ")
+Age = 2021 - int(birth_date)
+
+print(Age)
