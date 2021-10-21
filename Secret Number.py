8a1d38fdc2ef7de0647340f0237076fd3788999b
FaisalAhmed64 <mdfaisalahmed025@gmail.com>
2021-10-21 23:55:20 +0600

    Finding A Secret Number

---
+++ Secret Number.py
@@ -0,0 +1,12 @@
+Secret_number = 9
+guess_count = 0
+guess_limit = 3
+
+# while guess_count<guess_limit:
+guss = int(input("guess: "))
+guess_count += 1
+if guss == Secret_number:
+    print("you win")
+    break
+else:
+print("you failed")
