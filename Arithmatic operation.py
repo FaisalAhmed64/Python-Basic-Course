ae138a1d15501f930dd4652ff6dc1b651a43e5e0
FaisalAhmed64 <mdfaisalahmed025@gmail.com>
2021-10-21 23:46:05 +0600

    Arithmatic Opeartion

---
+++ Arithmatic operation.py
@@ -0,0 +1,21 @@
+import math
+
+print(10 // 3)
+print(10 % 3)
+print(10 ** 3)
+x = 10
+x -= 3
+print(x)
+x = 10 + 3 * 2
+print(x)
+x = (2 + 3) * 10 - 3
+print(x)
+
+x = 3.5
+print(round(x))
+x = -3.5
+print(abs(x))
+
+x = 2.9
+print(math.ceil(x))
+print(math.floor(x))
