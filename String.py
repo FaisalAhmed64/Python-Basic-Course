7bf7d5f8bdcacdab323eb28b80ed571b7299ed33
FaisalAhmed64 <mdfaisalahmed025@gmail.com>
2021-10-21 23:31:03 +0600

    String And String Functions

---
+++ String.py
@@ -0,0 +1,13 @@
+first_name = "jhon"
+last_name = "smith"
+message = f'{first_name} {last_name} is a coder '
+print(message)
+print(len(message))
+course = "python basic course"
+print(course.upper())
+print(course.lower())
+print(course.find("c"))
+print(course.replace("p", "C"))
+print("python" in course)
+print("cython" in course)
+print(course.title())
